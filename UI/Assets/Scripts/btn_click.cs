d0a32c3662f7b02ea359b9771c5aa36ba2279a17
ruiyongzhang <ruiyong.z@gmail.com>
2015-04-06 23:22:45 -0700

    button output

---
+++ UI/Assets/Scripts/btn_click.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class btn_click : MonoBehaviour {
+
+	public void click()
+	{
+		Debug.Log(gameObject.name + " Button was pressed");
+	}
+	
+}
+
+
