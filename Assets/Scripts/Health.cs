0dccf19aff4c7ae048561e718e43351b18c22bb8
Jordan Schwichtenberg <jordanschwichtenberg@gmail.com>
2015-04-14 19:53:26 -0700

    Add prey menu, resource menu, and more

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	// Current Health
+	[SerializeField]
+	int currentHealth = 100;
+
+	public void lowerHealth(int damage) {
+		// Subtract damage from current health
+		currentHealth -= damage;
+
+		Debug.Log ("Health now: " + currentHealth);
+
+		// Destroy object if health is depleted to 0
+		if (currentHealth <= 0) {
+			Destroy(gameObject);
+			Debug.Log(gameObject.ToString() + " has been eaten");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
